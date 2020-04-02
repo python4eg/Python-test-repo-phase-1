936161cd7384cd1c42611f6af86a56ce693fbd7b
PyThon <python4eg@users.noreply.github.com>
2020-04-02 20:05:54 +0300

    Update test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+# Наш перший коментар
+int_value = 1 # Ціле число, integer or int
+negative_int_value = -1 # Ціле відємне число, integer or int
+print(int_value)
+print(negative_int_value)
+
+float_value = 1.234 # Float, дробове число або чисо з плаваючою точкою
+print(float_value)
+
+string_value = 'Text' # String or str стрічка with quotes
+print(string_value)
+
+string_second = "Text 2" # String or str стрічка with double quotes 
+print(string_second)
+
+meaningful_sentence = "Once upon a time .... don't"
+print(meaningful_sentence)
+
+meaningful_sentence_two = 'Once upon a time .... don\'t  \\m/'
+print(meaningful_sentence_two)
+
+
+meaningful_sentence_three = 'The First line \n\tThe Second one'
+print(meaningful_sentence_three)
+
+
+very_long_string = 'Very long long long sentence without any sence or context but stil this one is very long and hardly readable'
+print(meaningful_sentence_three)
